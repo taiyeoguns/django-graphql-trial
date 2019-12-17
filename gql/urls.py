a0623b4a4763283b7e3edc268f1877e1030f5b23
Taiye <taiyeoguns@yahoo.com>
2019-12-17 06:08:59 +0000

    Update urls

--- gql/urls.py
+++ gql/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/graphql"), name="index"),
     path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(graphiql=True)),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
